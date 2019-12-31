aba59e65c7b1d13321c356b0b9434a47894b40ef
Shane Keney <shane@spikeball.com>
2019-12-31 13:48:50 -0500

    - save button functionality - will save to localStorage using textarea id & text as val - localStorage pull for each timeblock on  page open

--- scripts/index.js
+++ scripts/index.js
@@ -18,11 +18,31 @@ function createTimeBlocks() {
     for(let i=startWork; i < (startWork + numberOfHours + 1); i++) {
         let createTimeBlock = timeBlockTemplate.clone();
         let textDescString = militaryTimeConvert[i];
+        let timeBlockElID = timeText[i] + `timeBlock`;
+        let timeTextInputID = timeText[i] + `textInput`;
+        let timeButtonID = timeText[i] + `button`;
 
         createTimeBlock.find(".timeSlotDesc").text(textDescString);
+        createTimeBlock.attr("id", timeBlockElID);
+        createTimeBlock.find(".toDoTextArea").attr("id", timeTextInputID);
+        createTimeBlock.find(".saveButton").attr("id", timeButtonID);
+
+        //pull from localStorage current time plans
+        createTimeBlock.find(`#${timeTextInputID}`).val(localStorage.getItem(timeTextInputID));
+
+        createTimeBlock.find(".saveButton").on("click", saveNotes);
 
         timeBlockContainer.append(createTimeBlock);
     }
 
 
+}
+
+//function for saving notes using id of sibling textarea and value=text
+function saveNotes() {
+    let buttonPressed = $(this);
+    let textAreaEl = buttonPressed.parent().siblings(".textInputContainer").find("textarea");
+    let textInputID = textAreaEl.attr("id");
+
+    localStorage.setItem(textInputID, textAreaEl.val());
 }
